eef57fa885ea87d535804dcd2e302f7d54313f8c
Rohit Tokala <rtokala@purdue.edu>
2022-08-08 22:31:43 +0530

    Add comment action buttons

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -1,4 +1,31 @@
-export function Comment({ id, message }) {
-  console.log(message);
-  return <div>{message}</div>;
+import { IconBtn } from "./IconBtn";
+import { FaHeart, FaReply, FaEdit, FaTrash } from "react-icons/fa";
+
+const dateFormatter = new Intl.DateTimeFormat(undefined, {
+  dateStyle: "medium",
+  timeStyle: "short",
+});
+
+export function Comment({ id, message, user, createdAt }) {
+  return (
+    <>
+      <div className="comment">
+        <div className="header">
+          <span className="name">{user.name}</span>
+          <span className="date">
+            {dateFormatter.format(Date.parse(createdAt))}
+          </span>
+        </div>
+        <div className="message">{message}</div>
+        <div className="footer">
+          <IconBtn Icon={FaHeart} aria-label="Like">
+            2
+          </IconBtn>
+          <IconBtn Icon={FaReply} aria-label="Reply" />
+          <IconBtn Icon={FaEdit} aria-label="Edit" />
+          <IconBtn Icon={FaTrash} aria-label="Delete" color="danger" />
+        </div>
+      </div>
+    </>
+  );
 }
