95fb0205bc083e077ad426dd1c350d852e493fb2
Rohit Tokala <rtokala@purdue.edu>
2022-08-08 20:48:49 +0530

    Fix comment list not displaying

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -1,3 +1,4 @@
 export function Comment({ id, message }) {
+  console.log(message);
   return <div>{message}</div>;
 }
--- client/src/components/CommentList.js
+++ client/src/components/CommentList.js
@@ -2,8 +2,10 @@ import { Comment } from "./Comment";
 
 export function CommentList({ comments }) {
   return comments.map((comment) => {
-    <div key={comment.id} className="comment-stack">
-      <Comment {...comment} />
-    </div>;
+    return (
+      <div key={comment.id} className="comment-stack">
+        <Comment {...comment} />
+      </div>
+    );
   });
 }
